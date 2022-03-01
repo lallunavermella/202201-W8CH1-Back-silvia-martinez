e48428d12c46398030e1b78e3cbd64c91005b4e8
Silvia Martinez <sil@silvia.com>
2022-03-01 21:35:17 +0100

    Add Pokemon SSG

---
+++ pages/pokemonSSG/index.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+function PokemonsSSR({ results }) {
+  return (
+    <>
+      <div>
+        <ul>
+          {results &&
+            results.map((pokemon) => (
+              <li key={pokemon.name}>
+                <Image
+                  src={pokemon.image}
+                  alt={pokemon.name}
+                  width="100"
+                  height="100"
+                />
+                <p>{pokemon.name}</p>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(process.env.NEXT_PUBLIC_POKE);
+  const { results } = await response.json();
+
+  return {
+    props: { results },
+  };
+}
+
+export default PokemonsSSR;
