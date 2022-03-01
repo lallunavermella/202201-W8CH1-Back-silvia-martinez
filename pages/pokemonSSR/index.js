be3524095597dea9785fda194de0126e1381a0fd
Silvia Martinez <sil@silvia.com>
2022-03-01 21:20:21 +0100

    Change component to page in PokemonSSR

--- pages/pokemonSSR/index.js
+++ pages/pokemonSSR/index.js
@@ -1,6 +1,35 @@
-import PokemonSSR from "../../components/PokemonSSR/PokemonSSR";
+import Image from "next/image";
 
-const PokemonListSSR = () => {
-  return <PokemonSSR />;
-};
-export default PokemonListSSR;
+function PokemonsSSR({ results }) {
+  return (
+    <>
+      <div>
+        <ul>
+          {results &&
+            results.map((pokemon) => (
+              <li key={pokemon.name}>
+                <Image
+                  src={pokemon.image}
+                  alt={pokemon.name}
+                  width="100"
+                  height="100"
+                />
+                <p>{pokemon.name}</p>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(process.env.NEXT_PUBLIC_POKE);
+  const { results } = await response.json();
+
+  return {
+    props: { results },
+  };
+}
+
+export default PokemonsSSR;
