07d9375857c415f080ae6c4f0e5172160d3d6e78
Silvia Martinez <sil@silvia.com>
2022-03-01 21:51:04 +0100

    Added Pokemon ISR

---
+++ pages/pokemonISR/pokemonISR.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+function PokemonsISR({ results }) {
+  return (
+    <>
+      <h2>Pokemon ISR</h2>
+      <div>
+        <ul>
+          {results &&
+            results.map((pokemon) => (
+              <li key={pokemon.name}>
+                <Image
+                  src={pokemon.image}
+                  alt={pokemon.name}
+                  width="100"
+                  height="100"
+                />
+                <p>{pokemon.name}</p>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(process.env.NEXT_PUBLIC_POKE);
+  const { results } = await response.json();
+
+  return {
+    props: { results },
+    revalidate: 20,
+  };
+}
+
+export default PokemonsISR;
